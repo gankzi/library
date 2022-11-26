b84d1ba3f41e3dca428ef4c9c79a2d7dc850720a
Gankzi <hoonkang1024@gmail.com>
2022-11-25 18:19:43 -0800

    Update book creation and delete function
    Updated addbooktolibrary function to add book when user creates
    new book with their input and deletes the book chosen when
    delete button is clicked.

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let addBook = document.querySelector('#add-book');
+let form = document.querySelector(".form-container");
+let library = document.querySelector("#book-container");
+let titleInput = document.querySelector("#book-title");
+let authorInput = document.querySelector("#book-author");
+let pagesInput = document.querySelector("#book-pages");
+let readInput = document.querySelector("#read-or-not");
+let createBook = document.querySelector(".create-btn");
+
+createBook.addEventListener("click", addBookToLibrary);
+
+
+
 
 let myLibrary = [];
 
@@ -9,12 +20,20 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-Book.prototype.info = function() {
-    console.log(this.title + " by " + this.author + ", " + this.pages + ", " + this.read);
-}
+function addBookToLibrary(event) {
+    let title = titleInput.value;
+    let author = authorInput.value;
+    let pages = pagesInput.value;
+    let read = readInput.value; 
 
-function addBookToLibrary() {
+    event.preventDefault();
+
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
     
+    populateBooks();
+    clearForm();
+    closeForm();
 }
 
 
@@ -25,4 +44,65 @@ function openForm() {
 function closeForm() {
     document.querySelector("#new-book").style.display = "none";
 }
+
+function populateBooks() {
+    
+        const cards = document.querySelectorAll(".card");
+        cards.forEach(card => library.removeChild(card));
+
+
+        for(i = 0; i < myLibrary.length; i++) {
+        const cardDiv = document.createElement("div");
+        const title = document.createElement("h4");
+        const author = document.createElement("h4");
+        const pages = document.createElement("h4");
+        const read = document.createElement("button");
+        const deleteBk = document.createElement("button");
+
+        read.classList.add("read-toggle");
+        read.addEventListener("click", toggleRead);
+        deleteBk.textContent = "Delete";
+        deleteBk.classList.add("del-book");
+        deleteBk.addEventListener("click", deleteBook)
+        cardDiv.classList.add("card");
+        cardDiv.setAttribute("id", i);
+        deleteBk.setAttribute("id", i);
+
+        title.textContent = myLibrary[i].title;
+        author.textContent = myLibrary[i].author;
+        pages.textContent = myLibrary[i].pages;
+        read.textContent = myLibrary[i].read;
+
+        library.appendChild(cardDiv);
+        cardDiv.appendChild(title);
+        cardDiv.appendChild(author);
+        cardDiv.appendChild(pages);
+        cardDiv.appendChild(read);
+        cardDiv.appendChild(deleteBk);
+
+        };
+    
+}
+
+function clearForm() {
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+}
+
+function deleteBook(event) {
+    const index = event.target.id;
+
+    myLibrary.splice(index, 1);
+    populateBooks();
+}
+
+function toggleRead() {
+    
+}
+
+
+
+
+
 
